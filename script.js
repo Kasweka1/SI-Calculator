ec09619e684ed6d44af2b6c072004fe63f5b7038
Kasweka Michael Mukoko <71525985+Kasweka1@users.noreply.github.com>
2021-08-09 18:43:57 +0200

    completed markup, styling and logic

---
+++ script.js
@@ -0,0 +1,26 @@
+const answer = document.getElementById("answer");
+const form = document.getElementById('value');
+
+form.addEventListener("submit", e=>{
+	e.preventDefault();
+
+	// Value Variables
+	const principle = document.getElementById("principle").value;
+	const rate = document.getElementById("rate").value;
+	const time = document.getElementById("time").value;
+	
+	// array for values
+	values = [principle, rate, time];
+
+	// for loop to iterate value and to check whether there are numbers
+	for (var i = 0; i < values.length; i++) {
+		if(isNaN(values[i])){
+			alert("Please input a valid Number");
+		}else{
+			let result = (principle * rate * time) / 100;
+			answer.innerHTML = result;
+		}
+	}
+	
+})
+
